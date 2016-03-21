11d1329b0d82fbd25e5f593ea5800c08c782e3d8
Jacob Porter Herold <dcejake@gmail.com>
2016-03-21 09:36:40 -0700

    Initial commit for gulpFramework

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var compute = require('../scripts/app.js');
+
+describe('My first test', function() {
+  it('should check first question', function() {
+    assert.deepEqual(compute.getQuestion(0), 'first');
+  });
+
+  it('should check third question', function() {
+    assert.deepEqual(compute.getQuestion(2), 'third');
+  });
+
+  it('should increment 0 to get 5', function() { assert(5 == compute.addFive(0)); });
+  it('should increment 4 to get 9', function() { assert(9 == compute.addFive(4)); });
+  it('should increment -1008 to get -1003', function() { assert(compute.addFive(-1008) == -1003); });
+});
